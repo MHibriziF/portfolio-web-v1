df5c41991c527465cc9c682e8d2fda2b41b4f207
MHibriziF <hibrizifarghana@gmail.com>
2024-08-17 14:11:35 +0700

    Turn navlink to cyan when hovered

--- src/components/navigation/Navlink.jsx
+++ src/components/navigation/Navlink.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navlink(props) {
   return (
     <li>
-      <a href={props.href} className="mx-4 hover:scale-110 ">
+      <a href={props.href} className="mx-4 hover:text-cyan-100 ">
         {props.text}
       </a>
     </li>
