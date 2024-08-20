9d0f0f8a1557b75699c4baeed9a49c3187f55475
MHibriziF <hibrizifarghana@gmail.com>
2024-08-20 21:47:31 +0700

    Finish responsiveness and update Netco Logo

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import SmallTitle from "./sub-components/SmallTitle";
-import CircleImg from "./sub-components/CircleImg";
-import Bulletpoint from "./sub-components/Bulletpoint";
 import Descriptor from "./sub-components/Descriptor";
 
 function Education() {
   return (
     <section className="pt-1 flex justify-center text-white">
-      <div className="w-full flex flex-col">
+      <div
+        className="w-full flex flex-col"
+        data-aos="zoom-in"
+        data-aos-duration="2000"
+      >
         <SmallTitle text="Education" />
         <Descriptor
           start="left"
           img="makara-ui.png"
-          bullet="Information System --- 2023 — 2027 (expected)"
-          text="Current GPA: 3.94 / 4.0 
+          bullet="Universitas Indonesia — Undergraduate Information System"
+          text="
+                Current GPA: 3.94 / 4.0 
                 Credits Taken: 42"
         />
         <Descriptor
           start="right"
-          img="logo-netco.jpg"
-          bullet="Science Student --- 2020 — 2023"
-          text="Activities: Netco Informatics Olympiad Team, English Club
+          img="logo-netco.jpeg"
+          bullet="SMAN 3 Kota Jambi — Science Student"
+          text="
+                Activities: Netco Informatics Olympiad Team, English Club
                 Achievements: 7th Place In the Jambi City Informatics Olympiad"
         />
       </div>
--- src/components/sub-components/Descriptor.jsx
+++ src/components/sub-components/Descriptor.jsx
@@ -6,7 +6,13 @@ function Descriptor({ start, text, img, bullet }) {
   if (start === "left") {
     return (
       <div className="flex spc:flex-row flex-col justify-start items-center mb-4">
-        <CircleImg size="small" img={img} />
+        <div
+          data-aos="zoom-in-right"
+          data-aos-duration="2000"
+          className="m-0 p-0"
+        >
+          <CircleImg size="small" img={img} />
+        </div>
         <div className="grid grid-cols-[2rem_1fr] grid-rows-[2rem_1fr] font-poppins spc:text-base text-sm">
           <Bulletpoint text={bullet} start={start} />
           <div className="relative flex ml-16 spc:w-[40rem] w-72 h-36 row-span-2 group">
@@ -26,14 +32,20 @@ function Descriptor({ start, text, img, bullet }) {
   } else if (start === "right") {
     return (
       <div className="flex spc:flex-row-reverse flex-col justify-start items-center mb-4">
-        <CircleImg size="small" img={img} />
+        <div
+          data-aos="zoom-in-left"
+          data-aos-duration="2000"
+          className="m-0 p-0"
+        >
+          <CircleImg size="small" img={img} />
+        </div>
         <div className="flex flex-col items-start font-poppins spc:text-base text-sm">
           <div className="flex w-full justify-end">
             <Bulletpoint text={bullet} start={start} />
           </div>
 
-          <div className="relative spc:w-[40rem] w-72 h-36 group">
-            <div className="absolute right-0 w-4 mt-4 h-32 rounded-full bg-slate-700 group-hover:rounded-3xl spc:group-hover:w-[37rem] group-hover:w-72 transition-all duration-500"></div>
+          <div className="relative ml-4 spc:w-[40rem] w-72 h-36 group">
+            <div className="absolute spc:mr-4 right-0 w-4 mt-4 h-32 rounded-full bg-slate-700 group-hover:rounded-3xl spc:group-hover:w-[37rem] group-hover:w-72 transition-all duration-500"></div>
             <p className="relative mr-16 mt-4 group-hover:mr-8 group-hover:font-semibold transition-all duration-500 text-right">
               {text.split("\n").map((line, index) => (
                 <React.Fragment key={index}>
